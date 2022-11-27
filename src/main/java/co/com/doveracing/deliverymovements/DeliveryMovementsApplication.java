75ddcc7e430d96914aec0f3ccab3e092d28e93c7
Jose Osorio Quintero <josquin@outlook.com>
2022-11-26 22:22:18 -0500

    primer construcciÃon proyecto

---
+++ src/main/java/co/com/doveracing/deliverymovements/DeliveryMovementsApplication.java
@@ -0,0 +1,13 @@
+package co.com.doveracing.deliverymovements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryMovementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryMovementsApplication.class, args);
+	}
+
+}
